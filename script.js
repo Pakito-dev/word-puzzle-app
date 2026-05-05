c63639983055bc1148d1b9564af6a9b783434e31
Pakito-dev <pajapaja00@gmail.com>
2026-05-05 16:56:12 +0200

    Adding basics. Checking vercel works.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function sayHello() {
     alert("It works!");
 }
+
+function getTodayWord() {
+    const words = ["apple", "grape", "melon"];
+    const day = new Date().getDate();
+    return words[day % words.length];
+}
+
+function check() {
+    const input = document.getElementById("guess").value;
+    const answer = getTodayWord();
+
+    if (input === answer) {
+        document.getElementById("result").innerText = "Success!";
+    } else {
+        document.getElementById("result").innerText = "Try again!";
+    }
+}
